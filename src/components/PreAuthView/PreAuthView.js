853b3b5d1ed8386bbd856af6400eb7c092940b73
Ryland Bell <ryland.bell@gmail.com>
2017-05-06 15:53:36 -0700

    add basic auth button

---
+++ src/components/PreAuthView/PreAuthView.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './PreAuthView.css';
+
+const clientID = '74436b5dd9624f8782f138387e69daaf';
+const redirectURI = 'http://localhost:3000';
+const scopes = ['playlist-read-private','playlist-modify-private'];
+
+const authURI = 'https://accounts.spotify.com/authorize?'
+  + 'client_id=' + clientID
+  + '&redirect_uri=' + encodeURIComponent(redirectURI)
+  + '&response_type=token'
+  + '&scope=' + scopes.join('%20')
+  + '&show-dialog=true';
+
+console.log(authURI);
+
+class PreAuthView extends Component {
+  handleClick() {
+    window.location = authURI;
+  }
+
+  render() {
+    return (
+      <div className="PreAuthView">
+        <h3>You should authorize this app to access your Spotify account:</h3>
+        <button onClick={this.handleClick} type="button" className="btn btn-default">Authorize</button>
+      </div>
+    );
+  }
+}
+
+export default PreAuthView;
