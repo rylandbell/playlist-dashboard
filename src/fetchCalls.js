9e62f0ba863e9463ca3f9329aaae155b3bd46089
Ryland Bell <ryland.bell@gmail.com>
2017-05-08 11:32:10 -0700

    add fetch function for audio features

--- src/fetchCalls.js
+++ src/fetchCalls.js
@@ -36,11 +36,36 @@ fetchCalls.getTracks = function(store, data) {
   fetch(playlistURI, requestOptions)
     .then(res => res.json())
     .then(res => {
-      console.log('fetched tracks: ', res.items);
       store.dispatch({
         type: 'ADD_TRACKS_DATA',
         data: res.items
       });
+      this.getTrackFeatures(store, res.items);
+    })
+    .catch(console.log);
+}
+
+fetchCalls.getTrackFeatures = function(store, tracks) {
+  console.log('fetched trackssss: ', tracks);
+  //get track list from Spotify, dispatch to Redux store:
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + store.getState().accessToken
+    },
+  }
+
+  const trackIds = tracks.map(track => track.track.id).join(',');
+  const featuresURI = `https://api.spotify.com/v1/audio-features?ids=${trackIds}`;
+
+  fetch(featuresURI, requestOptions)
+    .then(res => res.json())
+    .then(res => {
+      // store.dispatch({
+      //   type: 'ADD_TRACKS_DATA',
+      //   data: res.items
+      // });
+      console.log(res);
     })
     .catch(console.log);
 }
