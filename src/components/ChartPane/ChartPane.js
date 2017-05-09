7c5b79bc5d12c81572dba78ac2a637c25b868206
Ryland Bell <ryland.bell@gmail.com>
2017-05-08 20:33:23 -0700

    more styling

--- src/components/ChartPane/ChartPane.js
+++ src/components/ChartPane/ChartPane.js
@@ -7,9 +7,13 @@ class TracksPane extends Component {
   render() {
     return (
       <div className="col-xs-12 chart__pane">
-        <div className="center-block">     
-          <FeaturesLineChart {...this.props} />
-        </div>
+        {/*<select className="form-control chart__filter-select center-block">
+          <option>Danceability</option>
+          <option>Instrumentalness</option>
+          <option>Valence</option>
+        </select>*/}
+        <h4 className="clearfix">Danceability:</h4>
+        <FeaturesLineChart {...this.props} />
       </div>
     );
   }
--- src/components/ChartPane/FeaturesLineChart.js
+++ src/components/ChartPane/FeaturesLineChart.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Legend} from 'recharts';
+import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip} from 'recharts';
 
 class FeaturesLineChart extends Component {
   render () {
     const data = this.props.reduxState.audioFeatures || [];
     return (
-      <ResponsiveContainer width="95%" height={300}>
+      <ResponsiveContainer width="95%" height={280}>
         <LineChart
           data={data}
         >
-          <XAxis stroke="#ebebeb" strokeWidth={2} />
+          <XAxis stroke="#ebebeb" strokeWidth={2} interval={4} />
           <YAxis stroke="#ebebeb" strokeWidth={2} domain={[0, 1]} />
           {/*<CartesianGrid strokeDasharray="10 3"/>*/}
           <Tooltip/>
-          <Legend />
-          <Line type="monotone" dataKey="danceability" stroke="#5bc0de" strokeWidth={3} dot={false} activeDot={{r: 8}}/>
+          <Line type="monotone" dataKey="danceability" stroke="#5bc0de" strokeWidth={3} dot={false} connectNulls={true} activeDot={{r: 8}}/>
           {/*<Line type="monotone" dataKey="valence" stroke="#82ca9d" strokeWidth={3} dot={false} activeDot={{r: 8}}/>*/}
           {/*<Line type="monotone" dataKey="valence" stroke="#82ca9d" />*/}
         </LineChart>
