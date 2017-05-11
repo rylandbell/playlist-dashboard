ce73c6ab994b8701d9152beaf5479b23eefd910b
Ryland Bell <ryland.bell@gmail.com>
2017-05-10 18:01:13 -0700

    add tooltip to ChartFilterCheckbox

--- src/components/ChartPane/ChartFilterCheckbox.js
+++ src/components/ChartPane/ChartFilterCheckbox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Tooltip from 'rc-tooltip';
+import 'rc-tooltip/assets/bootstrap.css';
 
 class ChartFilterCheckbox extends Component {
   constructor(props) {
@@ -16,7 +18,14 @@ class ChartFilterCheckbox extends Component {
     const filter = this.props.filter;
     return (
       <label className="checkbox-inline chart___checkbox" >
-        <span className={"fa fa-line-chart " + filter.name + ' ' + (filter.isGraphed ? 'is-graphed' : '')} aria-hidden="true"></span>
+        <Tooltip
+          placement="right"
+          overlay="Include in chart"
+          arrowContent={<div className="rc-tooltip-arrow-inner"></div>}
+          style={{fontWeight: 'bold'}}
+        >
+          <span className={"fa fa-line-chart " + filter.name + ' ' + (filter.isGraphed ? 'is-graphed' : '')} aria-hidden="true"></span>
+        </Tooltip>
         <input className="hidden" type="checkbox" checked={filter.isGraphed} value={filter.name} onChange={this.onChange} /> 
         {/*<span style={{fontWeight: 'bold'}}>Show in Chart</span>*/}
       </label>
