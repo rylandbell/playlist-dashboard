93c6f858c903391c19ce56d3b0cbee72a7e255ee
Ryland Bell <ryland.bell@gmail.com>
2017-05-20 17:40:20 -0700

    add and configure Sass

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import PreAuthView from './PreAuthView/PreAuthView';
 
+//load AuthView as separate module, once authorization has been established
 class AuthView extends React.Component {
   componentWillMount = () => {
     import('./AuthView/AuthView').then(Component => {
-      console.log('Component = ', Component);
       this.Component = Component;
       this.forceUpdate();
     })
