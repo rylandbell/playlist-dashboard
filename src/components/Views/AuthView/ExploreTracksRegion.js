e1e58b846effae88909a073b7d7cfadfae0f7aa6
Ryland Bell <ryland.bell@gmail.com>
2017-05-22 11:14:50 -0700

    convert ExploreTracksRegion to flex

--- src/components/Views/AuthView/ExploreTracksRegion.js
+++ src/components/Views/AuthView/ExploreTracksRegion.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import TracksPane from '../../Panes/TracksPane/TracksPane';
 import ChartPane from '../../Panes/ChartPane/ChartPane';
-import SelectedTracksCount from './SelectedTracksCount';
+// import SelectedTracksCount from './SelectedTracksCount';
 import {filterByFeatures} from '../../../helper';
 
 class ExploreTracksRegion extends Component {
@@ -14,12 +14,17 @@ class ExploreTracksRegion extends Component {
     const filteredFeaturesData = featuresData.filter(filterByFeatures.bind(this));
 
     return (
-      <div>
-        <div className="row">
+      <div className="auth-view__explore-tracks-flexbox">
+        <div className="row auth-view__playlist-title-row">
+          <div className="col-xs-12">
+            <h3 className="tracks__heading"> {this.props.reduxState.selectedPlaylist.name} </h3>
+          </div>
+        </div>
+        <div className="row auth-view__tracks-row">
           <TracksPane allFilters={allFilters} filteredTracks={filteredTracks} filteredFeaturesData={filteredFeaturesData} {...this.props} /> 
         </div>
-        <SelectedTracksCount filteredTracks={filteredTracks} featuresData={featuresData} {...this.props} />
-        <div className="row">
+        {/*<SelectedTracksCount filteredTracks={filteredTracks} featuresData={featuresData} {...this.props} />*/}
+        <div className="row auth-view__chart-row">
           <ChartPane filteredTracks={filteredTracks} filteredFeaturesData={filteredFeaturesData} {...this.props} />
         </div>
       </div>
