c67d0f10b0c276598539590dfa2658c7c5a68c98
Ryland Bell <ryland.bell@gmail.com>
2017-05-24 14:04:42 -0700

    debug bad display on load

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import PreAuthView from './Views/PreAuthView/PreAuthView';
+import AuthView from './Views/AuthView/AuthView';
 
 //load AuthView as separate module, once authorization has been established
-class AuthView extends React.Component {
-  componentWillMount = () => {
-    import('./Views/AuthView/AuthView').then(Component => {
-      this.Component = Component;
-      this.forceUpdate();
-    })
-  }
-  render = () => (
-    this.Component ? <this.Component.default {...this.props} /> : null
-  )
-}
+// class AuthView extends React.Component {
+//   componentWillMount = () => {
+//     import('./Views/AuthView/AuthView').then(Component => {
+//       this.Component = Component;
+//       this.forceUpdate();
+//     })
+//   }
+//   render = () => (
+//     this.Component ? <this.Component.default {...this.props} /> : null
+//   )
+// }
 
 class App extends Component {
   render() {
