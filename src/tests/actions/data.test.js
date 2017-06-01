f9c5fe39b0966385181f25f001a95415088607ee
Ryland Bell <ryland.bell@gmail.com>
2017-06-01 16:59:23 -0700

    add tests for data-adding action creators

---
+++ src/tests/actions/data.test.js
@@ -0,0 +1,17 @@
+import * as actions from '../../actions/data';
+
+const testData = {data: 'test'};
+
+describe('Data-adding actions', () => { 
+  it('should create an action to add fetched playlist data to store', () => {
+    expect(actions.addPlaylistsData(testData)).toMatchSnapshot();
+  });
+
+  it('should create an action to add fetched tracks data to store', () => {
+    expect(actions.addTracksData(testData)).toMatchSnapshot();
+  });
+
+  it('should create an action to add fetched audio features data to store', () => {
+    expect(actions.addFeaturesData(testData)).toMatchSnapshot();
+  });
+});
