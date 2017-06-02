eab653a783ed5ea6626862d4fbca82d433436658
Ryland Bell <ryland.bell@gmail.com>
2017-06-01 17:12:07 -0700

    add tests for general action creators

---
+++ src/tests/actions/actions.test.js
@@ -0,0 +1,47 @@
+import * as actions from '../../actions/actions';
+
+const testData = {data: 'test'};
+const testFilterIndex = 0;
+const testFilterValue = [0,1];
+
+describe('General actions', () => { 
+  it('should create an action to select a playlist', () => {
+    expect(actions.selectPlaylist(testData, true)).toMatchSnapshot();
+  });
+
+  it('should create an action to select a nav tab', () => {
+    expect(actions.setActiveTab('testTabName')).toMatchSnapshot();
+  });
+
+  it('should create an action to change playlist name input text', () => {
+    expect(actions.changeNameText('testInput')).toMatchSnapshot();
+  });
+
+  it('should create an action to update a filter\'s range value', () => {
+    expect(actions.updateFilter(testFilterIndex, testFilterValue)).toMatchSnapshot();
+  });
+
+  it('should create an action to indicate that a feature slider is being dragged', () => {
+    expect(actions.startDraggingFeatureSlider(testFilterIndex)).toMatchSnapshot();
+  });
+
+  it('should create an action to indicate that a feature slider has stopped being dragged', () => {
+    expect(actions.stopDraggingFeatureSlider()).toMatchSnapshot();
+  });
+
+  it('should create an action to toggle whether a feature is drawn in chart', () => {
+    expect(actions.toggleChartedFeature(testFilterIndex, false)).toMatchSnapshot();
+  });
+
+  it('should create an action to indicate that a track row is hovered', () => {
+    expect(actions.hoverOnTrack(testData)).toMatchSnapshot();
+  });
+
+  it('should create an action to indicate that the cursor has left the tracks table', () => {
+    expect(actions.clearHoveredTrack()).toMatchSnapshot();
+  });
+
+  it('should create an action to stop animating the chart', () => {
+    expect(actions.stopAnimatingChart()).toMatchSnapshot();
+  });
+});
